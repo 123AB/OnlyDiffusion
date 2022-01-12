0ba95d92b1900c2279d3a1e9cf5c98d05ff2f459
z.zhang-16@student.tudelft.nl <z.zhang-16@student.tudelft.nl>
2022-01-12 21:04:00 +0100

    add files

--- upload_diffusion_model_ins/diffusion_zhiyue_unequally_seed_improved.cpp
+++ upload_diffusion_model_ins/diffusion_zhiyue_unequally_seed_improved.cpp
@@ -321,9 +321,9 @@ int main(int argc, char* argv[]){
     //string fileName = "intensityl";
     //string fileName= "intensityc";
     //string fileName= "pagerankl";
-    string fileName = "hindexl";
+    //string fileName = "hindexl";
     //string fileName = "hindexc";
-    //string fileName = "indegreec";
+    string fileName = "indegreel";
     //string fileName = "pagerankc";
 
     int k = -1;
@@ -352,8 +352,8 @@ int main(int argc, char* argv[]){
     //string s = std::format("{:10}", "some_string");
     //int seed_size[10] = {5,10,20,50,100,200,500,1000};
     //int seed_size[4] = {75,125,150,175};
-    //int seed_size[7] = {50,75,100,125,150,175,200};
-    int seed_size[2] = {175,200};
+    int seed_size[7] = {50,75,100,125,150,175,200};
+    //int seed_size[2] = {175,200};
 
     //int seed_size[3] = {150,175,200};
     //int seed_size[3] = {175,200};
@@ -385,7 +385,7 @@ int main(int argc, char* argv[]){
     //int chunk_fileSize = totalFileNum / filePiece;
 
     //for (double ratio = 1.0; ratio<=1.0; ratio = ratio + 0.1){
-    for (int ratio_size_len = 10; ratio_size_len<sizeof(ratio_list)/sizeof(ratio_list[0]); ratio_size_len = ratio_size_len + 1){
+    for (int ratio_size_len = 0; ratio_size_len<sizeof(ratio_list)/sizeof(ratio_list[0]); ratio_size_len = ratio_size_len + 1){
         double ratio = ratio_list[ratio_size_len];
         //for (int influenced_ratio = 0; influenced_ratio<sizeof(seed_size)/sizeof(seed_size[0]); influenced_ratio = influenced_ratio + 1){
         for(int seed_size_len = 0; seed_size_len<sizeof(seed_size)/sizeof(seed_size[0]); seed_size_len = seed_size_len + 1){
@@ -440,9 +440,9 @@ int main(int argc, char* argv[]){
                 //oss << "../student_preprocess/diffusion/ins_month/comment_" << i << ".csv";
                 oss << "../student_preprocess/diffusion/ins_month/like_" << i << ".csv";
 
-                //oss_seed << "../student_preprocess/indegree_temporal_ins/indegree_diversity_seed/diversity-" << fileName << "-" << seed_size[seed_size_len] << "-" << strRatio << "-" << i << ".csv";
+                oss_seed << "../student_preprocess/indegree_temporal_ins/indegree_diversity_seed/diversity-" << fileName << "-" << seed_size[seed_size_len] << "-" << strRatio << "-" << i << ".csv";
                 //oss_seed << "../student_preprocess/intensity_temporal_ins/intensity_diversity_seed/diversity-" << fileName << "-" << seed_size[seed_size_len] << "-" << strRatio << "-" << i << ".csv";
-                oss_seed << "../student_preprocess/target_hindex_temporal_ins/diversity_month_hi_index_ins/diversity-" << fileName << "-" << seed_size[seed_size_len] << "-" << strRatio << "-" << i << ".csv";
+                //oss_seed << "../student_preprocess/target_hindex_temporal_ins/diversity_month_hi_index_ins/diversity-" << fileName << "-" << seed_size[seed_size_len] << "-" << strRatio << "-" << i << ".csv";
                 //oss_seed << "../student_preprocess/pagerank_diversity_seed/diversity-" << fileName << "-" << seed_size[seed_size_len] << "-" << strRatio << "-" << i << ".csv";
 
 
